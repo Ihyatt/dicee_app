ea658abf6d3c3dfefd062dae73fd1441323e19d9
Ihyatt <inas.raheema@gmail.com>
2023-06-19 22:08:52 -0700

    decoration added

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Text('Warm Enough'),
+        body: Container(
+          // backgroundColor: Color.fromARGB(255, 168, 109, 97),
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: <Color>[
+              Color.fromARGB(
+                255,
+                168,
+                109,
+                97,
+              ),
+              Color.fromARGB(
+                255,
+                223,
+                176,
+                166,
+              ),
+            ],
+          )),
+          child: const Center(
+            child: Text(
+              'Warm Enough',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+          ),
+        ),
       ),
     ),
   );
