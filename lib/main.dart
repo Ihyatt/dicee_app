433bd71bd4623822692da3c89deef679ce8dfc25
Ihyatt <inas.raheema@gmail.com>
2023-06-20 12:21:50 -0700

    put grasdient code in stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,39 +3,46 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        body: Container(
-          // backgroundColor: Color.fromARGB(255, 168, 109, 97),
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: <Color>[
-              Color.fromARGB(
-                255,
-                168,
-                109,
-                97,
-              ),
-              Color.fromARGB(
-                255,
-                223,
-                176,
-                166,
-              ),
-            ],
-          )),
-          child: const Center(
-            child: Text(
-              'Warm Enough',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ),
+      home: Scaffold(body: GradientContainer()),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  //GradientContainer is inheriting from StatelessWidget
+
+  @override //annotation to make it clear that you are overriding build from StatelessWidget
+  Widget build(context) {
+    return Container(
+      // backgroundColor: Color.fromARGB(255, 168, 109, 97),
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: <Color>[
+          Color.fromARGB(
+            255,
+            168,
+            109,
+            97,
+          ),
+          Color.fromARGB(
+            255,
+            223,
+            176,
+            166,
+          ),
+        ],
+      )),
+      child: const Center(
+        child: Text(
+          'Warm Enough',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
           ),
         ),
       ),
-    ),
-  );
+    );
+  } //build must return widget
 }
